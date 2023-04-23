985ffeb30b492824c347a23d07e4c6d4542b1eef
Alex <alexgrimes.se@gmail.com>
2023-04-23 14:27:44 -0400

    fix routing to a home page after sign in

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { supabase } from '../src/services/auth';
 import { AuthSession } from '@supabase/supabase-js';
 import Account from '../src/components/account';
 import Auth from '../src/components/login';
+import Home from '../src/pages/Home';
 
 const App: Component = () => {
   const [session, setSession] = createSignal<AuthSession | null>(null);
@@ -19,34 +20,9 @@ const App: Component = () => {
 
   return (
     <div class='container' style={{ padding: '50px 0 100px 0' }}>
-      {!session() ? <Auth /> : <Account session={session()!} />}
+      {!session() ? <Auth /> : <Home />}
     </div>
   );
 };
 
 export default App;
-// import logo from './logo.svg';
-// import styles from './App.module.css';
-
-// const App: Component = () => {
-//   return (
-//     <div class={styles.App}>
-//       <header class={styles.header}>
-//         <img src={logo} class={styles.logo} alt='logo' />
-//         <p class='text-blue-400'>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           class={styles.link}
-//           href='https://github.com/solidjs/solid'
-//           target='_blank'
-//           rel='noopener noreferrer'
-//         >
-//           Learn Solid
-//         </a>
-//       </header>
-//     </div>
-//   );
-// };
-
-// export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'solid-js';
+
+const Home: Component = () => {
+  return (
+    <div class='bg-black text-white'>
+      <h1>Home</h1>
+    </div>
+  );
+};
+export default Home;
